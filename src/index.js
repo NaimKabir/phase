9d925c8eea78afcb0ad7ba4fa58b2a771c8ac2fa
NaimKabir <kabir.naim@gmail.com>
2021-12-10 11:34:56 +0000

    quick poc for event listeners

--- src/index.js
+++ src/index.js
@@ -1,21 +1,34 @@
 import Vex from 'vexflow';
-const VF = Vex.Flow;
 
-// Create a VexFlow renderer attaced to the DIV element "boo"
-var vf = new VF.Factory({renderer: {elementId: 'boo'}});
-var score = vf.EasyScore();
-var system = vf.System();
+const ID = 'boo'; // vexflow element ID
 
-// Create a 4/4 treble stave, and add two parallel voices
-system.addStave({
-  voices: [
-    // Top voice has 4 quarter notes with stems up
-    score.voice(score.notes('C#5/q, B4, A4, G#4', {stem: 'up'})),
-   
-    // Bottom voice has two half notes, with the stem down
-    score.voice(score.notes('C#4/h, C#4', {stem: 'down'}))
-  ]
-}).addClef('treble').addTimeSignature('4/4');
+function renderStaves(id) {
+	const VF = Vex.Flow;
+	
+	// Create a VexFlow renderer attaced to the DIV element 
+	var vf = new VF.Factory({renderer: {elementId: id}});
+	var score = vf.EasyScore();
+	var system = vf.System();
+	
+	// Create a 4/4 treble stave, and add two parallel voices
+	system.addStave({
+	  voices: [
+	    // Top voice has 4 quarter notes with stems up
+	    score.voice(score.notes('C#5/q, B4, A4, G#4', {stem: 'up'})),
+	   
+	    // Bottom voice has two half notes, with the stem down
+	    score.voice(score.notes('C#4/h, C#4', {stem: 'down'}))
+	  ]
+	});
+	
+	// Draw it!
+	vf.draw();
+}
 
-// Draw it!
-vf.draw();
+function addHandlers(id) {
+	const E = document.getElementById(ID); // vexflow div
+	E.addEventListener('click', (e) => alert(e.type))
+}
+
+renderStaves(ID);
+addHandlers(ID);
