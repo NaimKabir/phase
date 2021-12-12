4fd9434c0e0da60bc5d892cb64fd1faa14c1a2e7
NaimKabir <kabir.naim@gmail.com>
2021-12-12 13:17:45 +0000

    one cursor note model to draw

--- src/mouseHandlers.js
+++ src/mouseHandlers.js
@@ -1,14 +1,16 @@
 import Vex from 'vexflow';
 
 export function getMouseOverHandler(context, stave) {
+	const CURSOR_NOTE = new Vex.Flow.StaveNote({keys:['g/4'], duration:'q'});
+	CURSOR_NOTE.setContext(context).setStave(stave);
+	CURSOR_NOTE.setTickContext(new Vex.Flow.TickContext());
 	return (e) => {
-		// Render a note ready to be placed somewhere on the stave.
+		// Render a CURSOR_NOTE ready to be placed somewhere on the stave.
 		let X = e.pageX;
 		let Y = e.pageY;
-		let note = new Vex.Flow.StaveNote({keys:['g/4'], duration:'q'});
-		note.setContext(context).setStave(stave).setTickContext(new Vex.Flow.TickContext());
-		note.getTickContext().setX(X);
-		note.draw();
-		console.log(e)
+		CURSOR_NOTE.getTickContext().setX(X);
+		context.clear();
+		CURSOR_NOTE.draw();
+		console.log(e);
 	}
 }
